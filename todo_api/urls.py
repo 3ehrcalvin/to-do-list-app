49dedd9decf63c12ebd4231e7e1159d79edf1a23
3ehrcalvin <calvin@3ecpa.com.sg>
2025-02-06 17:30:32 +0800

    fix: added url for fetching the api

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TodoApiView
+from .views import TodoApiView, TodoDetailApiView
 
 urlpatterns = [
-    path('api', TodoApiView.as_view())
+    path('api', TodoApiView.as_view()),
+    path('api/<int:todo_id>', TodoDetailApiView.as_view())
 ]
